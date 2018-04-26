8ad35298be0ccdbeafe5c6d11da06acfb6a83e7e
Ming <muvoid@foxmail.com>
2018-04-26 18:51:04 +0800

    *add doT.js loader

--- webpack.config.js
+++ webpack.config.js
@@ -108,6 +108,25 @@ module.exports = {
           },
         }],
 
+      }, {
+
+        // doT.js 模板引擎 loader
+        test: /\.dot$/,
+        loader: 'dotjs-loader',
+        options: {
+          evaluate:    /\{\{([\s\S]+?)\}\}/g,
+          interpolate: /\{\{=([\s\S]+?)\}\}/g,
+          encode:      /\{\{!([\s\S]+?)\}\}/g,
+          use:         /\{\{#([\s\S]+?)\}\}/g,
+          define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
+          conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
+          iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
+          varname: 'it',
+          strip: true,
+          append: true,
+          selfcontained: false
+        }
+
       },
     ]
   }
